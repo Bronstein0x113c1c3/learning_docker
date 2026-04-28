face53722c3fc201a0b712f57bf9743efcfe171d
jonathan0x113c1c3 <jonathanof2016@gmail.com>
2026-04-28 15:40:58 +0700

    epoch_1 done

--- d5/rust_test/epoch_1/build.nix
+++ d5/rust_test/epoch_1/build.nix
@@ -1,59 +1,200 @@
-{ pkgs ? import <nixpkgs> { }}:
-with pkgs;
-
-let
-    inherit (pkgs) lib;
-    fs = lib.fileset;
-    sourceFiles = ./.;
-    rust_app = rustPlatform.buildRustPackage{
-      name = "p";
-      version = "1.0";
-      src = fs.toSource {
-        root = ./.;
-        fileset = sourceFiles;
-    };
-      cargoHash = "sha256-EUwN7MI8Tbr1X2tdo+2Zkp/mT9Zs5EDqh93K9Bw0jbc=";
-    };
-
-
-
-
-#     go_app = stdenv.mkDerivation {
-#         name = "something";
-#         src = fs.toSource {
-#         root = ./.;
-#         fileset = sourceFiles;
+# # # { pkgs ? import <nixpkgs> { }}:
+# # # with pkgs;
+# # #
+# # # let
+# # #     inherit (pkgs) lib;
+# # #     fs = lib.fileset;
+# # #     sourceFiles = ./.;
+# # #
+# # #     rust_app = rustPlatform.buildRustPackage {
+# # #       name = "p";
+# # #       version = "1.0";
+# # #       src = fs.toSource {
+# # #         root = ./.;
+# # #         fileset = sourceFiles;
+# # #       };
+# # #
+# # #       cargoHash = "sha256-XPXxhCnIYT2Z1jJmdRlEKhpIRoRk6KAmnACiPsCPv68=";
+# # #
+# # #       # Di chuyển pkg-config vào đây
+# # #       nativeBuildInputs = [ pkg-config ];
+# # #
+# # #       # alsa-lib là thư viện liên kết (link), giữ nguyên ở buildInputs
+# # #       buildInputs = [ alsa-lib libpulseaudio ];
+# # #     };
+# # # in
+# # # #   rust_app
+# # #   dockerTools.buildImage {
+# # #   name = "my-rust-app-docker";
+# # #   tag = "latest";
+# # #
+# # #   # Các gói cần có trong runtime environment
+# # #   contents = [
+# # # #     pulseaudio
+# # # #     alsa-utils
+# # #     rust_app
+# # #     alsa-lib
+# # #     coreutils
+# # #     bashInteractive # Hữu ích để debug nếu cần
+# # #   ];
+# # #
+# # #   config = {
+# # #     Cmd = [ "${rust_app}/bin/epoch_1" ];
+# # #     # ALSA thường yêu cầu truy cập vào /dev/snd,
+# # #     # hãy nhớ chạy container với flag --device /dev/snd
+# # #     Env = [ "LD_LIBRARY_PATH=${alsa-lib}/lib" ];
+# # #   };
+# # #   }
+# #
+# #
+# # { pkgs ? import <nixpkgs> { }}:
+# # let
+# #   inherit (pkgs) lib dockerTools rustPlatform;
+# #
+# #   rust_app = rustPlatform.buildRustPackage {
+# #     name = "p";
+# #     version = "1.0";
+# #     src = lib.fileset.toSource {
+# #       root = ./.;
+# #       fileset = ./.;
+# #     };
+# #     cargoHash = "sha256-XPXxhCnIYT2Z1jJmdRlEKhpIRoRk6KAmnACiPsCPv68=";
+# #
+# #     nativeBuildInputs = [ pkgs.pkg-config ];
+# #     buildInputs = [ pkgs.alsa-lib pkgs.libpulseaudio ];
+# #   };
+# #
+# # in
+# # dockerTools.buildImage {
+# #   name = "my-rust-app-docker";
+# #   tag = "latest";
+# #
+# #   contents = [
+# #     rust_app
+# #     pkgs.alsa-lib
+# #     pkgs.libpulseaudio
+# #     pkgs.coreutils
+# #     pkgs.bashInteractive
+# # #     pkgs.cacert # Cần thiết nếu app có gọi HTTPS
+# #   ];
+# #
+# #   # Tạo cấu trúc file hệ thống cần thiết
+# #   extraCommands = ''
+# #     # Tạo thư mục tạm
+# #     mkdir -p tmp
+# #
+# #     # Tạo file group để Docker nhận diện được group 'audio' (GID 29 là chuẩn chung)
+# #     mkdir -p etc
+# #     echo "root:x:0:" > etc/group
+# #     echo "audio:x:17:" >> etc/group
+# #
+# #     # Copy cấu hình ALSA mặc định để thư viện không bị crash
+# #     mkdir -p etc/alsa
+# #     cp -r ${pkgs.alsa-lib}/share/alsa/* etc/alsa/
+# #   '';
+# #
+# #   config = {
+# #     Cmd = [ "${rust_app}/bin/epoch_1" ];
+# #     Env = [
+# #       # Chỉ định đường dẫn thư viện cho Runtime
+# #       "LD_LIBRARY_PATH=${pkgs.alsa-lib}/lib:${pkgs.libpulseaudio}/lib"
+# #       # Chỉ định nơi tìm config ALSA
+# #       "ALSA_CONFIG_PATH=/etc/alsa/alsa.conf"
+# #     ];
+# #   };
+# # }
+# #
+#
+# { pkgs ? import <nixpkgs> { }}:
+# let
+#   inherit (pkgs) lib dockerTools rustPlatform;
+#
+#   rust_app = rustPlatform.buildRustPackage {
+#     name = "p";
+#     version = "1.0";
+#     src = lib.fileset.toSource {
+#       root = ./.;
+#       fileset = ./.;
 #     };
-#         nativeBuildInputs = [ pkg-config alsa-lib go gcc ];
-#         unpackPhase = " ls  && go mod tidy && go build -o main ";
-#         installPhase = "
-#             mkdir -p $out/bin
-#             cp hello $out/bin/
-#         ";
-#     };
-in
+#     cargoHash = "sha256-XPXxhCnIYT2Z1jJmdRlEKhpIRoRk6KAmnACiPsCPv68=";
+#     nativeBuildInputs = [ pkgs.pkg-config ];
+#     buildInputs = [ pkgs.alsa-lib pkgs.libpulseaudio ];
+#   };
+#
+# in
+# dockerTools.buildImage {
+#   name = "my-rust-app-docker";
+#   tag = "latest";
+#
+#   contents = [
 #     rust_app
+#     pkgs.alsa-lib
+#     pkgs.coreutils
+#     pkgs.bashInteractive
+#   ];
+#
+#   extraCommands = ''
+#     mkdir -p etc
+#     # Tạo group audio (kiểm tra lại GID máy host bằng lệnh: getent group audio)
+#     echo "root:x:0:" > etc/group
+#     echo "audio:x:17:" >> etc/group
+#
+#     # QUAN TRỌNG: Cấu hình ALSA để nhận diện thiết bị mặc định
+#     cat > etc/asound.conf <<EOF
+# pcm.!default {
+#     type hw
+#     card 0
+# }
+# ctl.!default {
+#     type hw
+#     card 0
+# }
+# EOF
+#   '';
+#
+#   config = {
+#     Cmd = [ "${rust_app}/bin/epoch_1" ];
+#     Env = [
+#       "LD_LIBRARY_PATH=${pkgs.alsa-lib}/lib"
+#       # Chỉ định ALSA đọc file config chúng ta vừa tạo
+#       "ALSA_CONFIG_PATH=/etc/asound.conf"
+#     ];
+#   };
+# }
+#
+#
+{ pkgs ? import <nixpkgs> { }}:
+let
+  inherit (pkgs) lib dockerTools rustPlatform;
 
-#     go_app
-  dockerTools.buildImage {
+  rust_app = rustPlatform.buildRustPackage {
+    name = "p";
+    version = "1.0";
+    src = ./.;
+    cargoHash = "sha256-XPXxhCnIYT2Z1jJmdRlEKhpIRoRk6KAmnACiPsCPv68=";
+    nativeBuildInputs = [ pkgs.pkg-config ];
+    buildInputs = [ pkgs.alsa-lib ];
+  };
+
+in
+dockerTools.buildImage {
   name = "my-rust-app-docker";
   tag = "latest";
-
-  # Các gói cần có trong runtime environment
   contents = [
     rust_app
-#     alsa-lib
-    coreutils
-    bashInteractive # Hữu ích để debug nếu cần
-  ];
-
+    pkgs.alsa-lib
+    pkgs.coreutils
+    pkgs.bashInteractive
+    ];
+  extraCommands = ''
+    mkdir -p etc
+    mkdir list_songs
+    ls
+    echo "root:x:0:" > etc/group
+    echo "audio:x:17:" >> etc/group # Thay 29 bằng GID máy bạn
+  '';
   config = {
     Cmd = [ "${rust_app}/bin/epoch_1" ];
-    # ALSA thường yêu cầu truy cập vào /dev/snd,
-    # hãy nhớ chạy container với flag --device /dev/snd
-#     Env = [ "LD_LIBRARY_PATH=${alsa-lib}/lib" ];
+    Env = [ "LD_LIBRARY_PATH=${pkgs.alsa-lib}/lib" ];
   };
-  }
-
-
-
+}
--- d5/rust_test/epoch_1/src/main.rs
+++ d5/rust_test/epoch_1/src/main.rs
@@ -1,6 +1,213 @@
-use tokio::*;
+use std::sync::{Arc, Mutex};
 
+use tokio::{self, sync::mpsc::unbounded_channel};
+
+use rodio::{Decoder, OutputStream, Sink};
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // use tokio::io::{self, AsyncBufReadExt};
+
+    // let stdin = io::stdin();
+    // let mut reader = io::BufReader::new(stdin).lines();
+
+    // println!("Type something and press Enter (Ctrl+D to exit):");
+
+    // while let Ok(Some(line)) = reader.next_line().await {
+    //     println!("You typed: {}", line);
+    // }
+
+    /* */
+    let (stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    // stream.into()
+    let sink = Arc::new(Mutex::new(Sink::try_new(&stream_handle).unwrap()));
+
+    let (mut tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+    // Add a oneshot channel for completion notification
+    let (complete_tx, complete_rx) = tokio::sync::oneshot::channel();
+
+    let file = std::io::BufReader::new(
+        std::fs::File::open("./list_songs/02. Why Why Why.flac").unwrap(),
+    );
+    let mut source = Decoder::new(file).unwrap();
+
+    // Producer task
+    let t = tokio::spawn(async move {
+        let buffer: Vec<i16> = source.by_ref().collect();
+        let bytes: Vec<u8> = buffer
+        .iter()
+        .flat_map(|&num| num.to_be_bytes().to_vec())
+        .collect();
+        let mut iter = bytes.chunks(16384);
+
+        while let Some(res) = iter.next() {
+            let mut salt = "music_chunk".as_bytes().to_vec();
+            let mut res = res.to_owned();
+            res.append(&mut salt);
+            tx.send(res);
+        }
+
+        // Signal that we've finished sending all data
+    });
+
+    // Consumer task
+    let s1 = sink.clone();
+    let t3 = tokio::spawn(async move {
+        while let Some(res) = rx.recv().await {
+            println!("receiving chunk...");
+            let res: Vec<i16> = res[..res.len() - 11]
+            .chunks_exact(2)
+            .map(|chunk| i16::from_be_bytes(chunk.try_into().unwrap()))
+            .collect();
+            let res = rodio::buffer::SamplesBuffer::new(2, 48000, res);
+            s1.lock().unwrap().append(res);
+        }
+        complete_tx.send(()).unwrap();
+    });
+
+    // Player wait task
+    let t2 = tokio::spawn(async move {
+        // Wait for either:
+        // 1. The completion signal (all data sent)
+        // 2. The consumer task to finish (in case of error)
+        tokio::select! {
+            _ = complete_rx => {
+                println!("All data received, waiting for playback to finish...");
+                sink.lock().unwrap().sleep_until_end();
+                println!("Playback completed!");
+            }
+            // _ = t3 => {
+            //     println!("Consumer task finished early");
+            // }
+        }
+    });
+
+    // Wait for all tasks to complete
+    tokio::try_join!(t, t3, t2).unwrap();
 }
+
+// #[tokio::test]
+// async fn comparing() {
+//     let mut vec1 = Vec::with_capacity(300);
+//     let mut vec2 = vec![4, 5, 6, 8];
+//
+//     {
+//         vec1 = vec2;
+//     }
+//     // (&mut vec1) = vec2;
+//     println!("{:?}", vec1.len());
+// }
+//
+// #[tokio::test]
+// async fn test_equal() {
+//     let test_str = "done".as_bytes().to_vec();
+//     assert!(&test_str == "done".as_bytes());
+// }
+//
+// #[tokio::test]
+// async fn test_some() {
+//     let buffer: Vec<i16> = vec![-1, 34, 56, 23, 32535, 0];
+//     let expected: Vec<u8> = vec![255, 255, 0, 34, 0, 56, 0, 23, 127, 23, 0, 0];
+//     let bytes: Vec<u8> = buffer
+//     .iter()
+//     .flat_map(|&num| num.to_be_bytes().to_vec()) // little-endian
+//     // .flat_map(|&num| num.to_be_bytes().to_vec()) // big-endian
+//     .collect();
+//     // assert!(bytes,"{}", expected);
+//     for i in 0..expected.len() {
+//         assert!(expected[i] == bytes[i], "done!!");
+//     }
+//     // println!("{:?}", bytes);
+//     // println!("{:?}", expected);
+// }
+//
+// #[tokio::test]
+//
+// async fn test_transferring() {
+//     // println!("{}",str);
+//     let (stream, stream_handle) = OutputStream::try_default().unwrap();
+//
+//     let sink = Arc::new(Mutex::new(Sink::try_new(&stream_handle).unwrap()));
+//     let (mut tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+//     let t = tokio::spawn(async move {
+//         let file = std::io::BufReader::new(
+//             std::fs::File::open("./list_songs/02 - Good To Be.flac").unwrap(),
+//         );
+//         let mut source = Decoder::new(file).unwrap();
+//
+//         // source.
+//         let buffer: Vec<i16> = source.by_ref().collect();
+//         // println!("{:?}", &buffer);
+//         let bytes: Vec<u8> = buffer
+//         .iter()
+//         .flat_map(|&num| num.to_be_bytes().to_vec()) // little-endian
+//         // .flat_map(|&num| num.to_be_bytes().to_vec()) // big-endian
+//         .collect();
+//         let mut iter = bytes.chunks(2048); // 1mb per chunk....
+//
+//         while let Some(res) = iter.next() {
+//             tx.send(res.to_owned());
+//         }
+//     });
+//     let s1 = sink.clone();
+//
+//     let t3 = tokio::spawn(async move {
+//         while let Some(res) = rx.recv().await {
+//             // socket.send_to(&res, addr).await.unwrap();
+//             // println!("the receiver: {:?}",res);
+//             // assert!()
+//             println!("receiving chunk...");
+//             let res: Vec<i16> = res
+//             .chunks_exact(2)
+//             .map(|chunk| i16::from_be_bytes(chunk.try_into().unwrap()))
+//             .collect();
+//             let res = rodio::buffer::SamplesBuffer::new(2, 48000, res);
+//             s1.lock().unwrap().append(res);
+//         }
+//     });
+//     let t2 = tokio::task::spawn(async move {
+//         sink.lock().unwrap().sleep_until_end();
+//         println!("waiting is called!!!!");
+//         // tokio::task::yield_now().await;
+//         println!("now, yield...");
+//         println!("the player has been done!!!!")
+//     });
+//
+//     t.await;
+//     t2.await;
+//     t3.await;
+// }
+// #[tokio::test]
+// async fn test_eq() {
+//     let res = "abcxyz".to_string();
+//     assert!(&res[..3] != "abc", "they equals");
+// }
+//
+// #[tokio::test]
+// async fn test_plus() {
+//     let mut i: u8 = 255;
+//     i = i + 1;
+//     assert!(i == 0);
+// }
+//
+// #[tokio::test]
+// async fn test_close() {
+//     let (mut tx, mut rx) = unbounded_channel();
+//     let t1 = tokio::spawn(async move {
+//         while !tx.is_closed() {
+//             tx.send(1);
+//         }
+//         drop(tx);
+//     });
+//
+//     for i in 0..3 {
+//         match rx.recv().await {
+//             Some(i) => {
+//                 println!("{}", i);
+//             }
+//             None => {}
+//         }
+//     }
+//     rx.close();
+//     t1.await;
+// }
